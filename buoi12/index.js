62edad6b99424498c528ba43ad59a78b5609cdeb
taiphan2288 <67281521+taiphan2288@users.noreply.github.com>
2022-03-15 15:50:18 +0700

    fix

--- buoi12/index.js
+++ buoi12/index.js
@@ -1,9 +1,8 @@
-// Call libraries
 var readlineSync = require("readline-sync");
-const shortid = require("shortid");
+var shortid = require("shortid");
 var fs = require("fs");
-const { log } = require("util");
 
+var sexGlobal = ["male", "female"];
 var showMenu = function () {
   console.log("               Student Managerment                  ");
   console.log("====================================================");
@@ -19,62 +18,151 @@ var showMenu = function () {
   console.log(" 10.Exit");
 };
 
-function showStudents() {
-  // to read 'input.txt' file
-  const data = fs.readFileSync("./dataJson.txt", {
-    encoding: "utf8",
-    flag: "r",
-  });
-  console.log(data);
-}
+showMenu();
+var choise = readlineSync.question("Your choise? ");
+let studentFile = fs.readFileSync("./data.txt", "utf8");
+let student = JSON.parse(studentFile);
 
-var flag = true;
-while (flag) {
-  showMenu();
-  const choice = readlineSync.question("Your choice: ");
-  let test = parseInt(choice);
-  switch (test) {
+const createStudent = () => {
+  let name = readlineSync.question("name? ");
+  let age = readlineSync.question("age? ");
+  let sex = readlineSync.keyInSelect(sexGlobal, "sex? male/female");
+  student.push({
+    id: shortid.generate(),
+    name: name,
+    age: parseInt(age),
+    sex: sexGlobal[sex],
+  });
+
+  saveFile();
+  choise = 0;
+};
+const saveFile = () => {
+  fs.writeFileSync(
+    "./data.txt",
+    JSON.stringify(student.length > 0 ? student : []),
+    "utf8"
+  );
+};
+const deleteStudent = () => {
+  // delete name
+  let nameDel = readlineSync.question("What name do you want delete? ");
+  let indexDel = student.findIndex((item) => {
+    return item.name.toLowerCase() === nameDel.toLowerCase();
+  });
+  if (indexDel >= 0) {
+    student.splice(indexDel, 1);
+    saveFile();
+  }
+  choise = 0;
+};
+const editStudent = () => {
+  let nameEdit = readlineSync.question("What name do you want edit? ");
+  let indexEdit = student.findIndex((item) => {
+    return item.name.toLowerCase() === nameEdit.toLowerCase();
+  });
+  if (indexEdit >= 0) {
+    let oldName = student[indexEdit].name;
+    let oldid = student[indexEdit].id;
+    let newAge = parseInt(readlineSync.question("age? "));
+    let sex = readlineSync.keyInSelect(sexGlobal, "sex? male/female");
+    let newStudent = {
+      id: oldid,
+      name: oldName,
+      age: newAge,
+      sex: sexGlobal[sex],
+    };
+    student.splice(indexEdit, 1, newStudent);
+    saveFile();
+    choise = 0;
+  }
+};
+
+const filterStudent = () => {
+  let nameFilter = readlineSync
+    .question("What name do you want find?")
+    .toLowerCase();
+  let listFilter = student.filter((item) => {
+    return item.name.toLowerCase() === nameFilter;
+  });
+  console.log(listFilter);
+  choise = 0;
+};
+const sortByName = () => {
+  // sort không dấu
+  student.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
+
+  //   sort có dấu ( test cho tiếng việt )
+  //   student.sort((a, b) => {
+  //     return a.name.localeCompare(b.name);
+  //   });
+  console.log(student);
+  choise = 0;
+};
+const sortByAge = () => {
+  // sort không dấu
+  student.sort((a, b) => a.age - b.age);
+  console.log(student);
+  choise = 0;
+};
+
+const deleAllStudent = () => {
+  student = [];
+  saveFile();
+  choise = 0;
+};
+
+const sumAgeStudent = () => {
+  let sum = student.reduce((a, b) => {
+    return (a += b.age);
+  }, 0);
+  console.log("Sum student", sum);
+  choise = 0;
+};
+
+while (true) {
+  switch (parseInt(choise)) {
+    case 0:
+      showMenu();
+      choise = readlineSync.question("Your choise? ");
+      break;
     case 1:
-      console.log(showStudents());
-      flag = true;
+      console.log(student);
+      choise = 0;
       break;
     case 2:
-      console.log(createStudents());
-      flag = true;
+      createStudent();
       break;
     case 3:
-      console.log(deleteStudents());
-      flag = true;
+      deleteStudent();
       break;
     case 4:
-      console.log(editStudents());
-      flag = true;
+      editStudent();
       break;
     case 5:
-      console.log(findStudents());
-      flag = true;
+      filterStudent();
       break;
     case 6:
-      console.log(sortNameStudents());
-      flag = true;
+      sortByName();
       break;
     case 7:
-      console.log(sortAgeStudents());
+      sortByAge();
       break;
     case 8:
-      console.log(deleteAllStudents());
-      flag = true;
+      deleAllStudent();
       break;
     case 9:
-      console.log(sumAgeStudents());
-      flag = true;
+      sumAgeStudent();
       break;
     case 10:
-      console.log("Exit");
-      flag = false;
-      break;
+      process.exit();
     default:
-      console.log("Please input valid option!");
+      console.log("Bạn phải chọn 1 số trong menu");
+      choise = 0;
       break;
   }
 }
